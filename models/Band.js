cf9045bfb9e86879b4af2f2344d797021f059fb2
med4h <adrian.meda6@gmail.com>
2024-11-07 11:11:06 -0800

    added bands & musician models and test

--- models/Band.js
+++ models/Band.js
@@ -1,7 +1,21 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Band model
-let Band;
+let Band = 
+
+class Band extends Model {}
+
+
+Bands.init({
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING,
+},{
+    sequelize,
+    modelName: 'Band'
+});
+
+
 
 module.exports = {
     Band
